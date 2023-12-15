287c41b6b95abc5bf4329773fe7a5965d24c6d53
apala <apala@10.164.59.75>
2023-12-15 13:44:58 +0100

    Ejercicios práctica 3-4

--- src/cap03_4/Recursividad.java
+++ src/cap03_4/Recursividad.java
@@ -3,49 +3,76 @@ package cap03_4;
 public class Recursividad {
 	
 	public static void main(String[] args) {
-		invertirFrase1("Hola", "", 3);
-		String frase = invertirFrase2("Holaaaaa");
+		
+		System.out.println("Pruebas ejercicio 1:");
+		String frase = invertirFrase1("Holaaaaa");
+		System.out.println(frase);
+		frase = invertirFrase2("Holaa12 2123");
+		System.out.println(frase);
+		frase = invertirFrase3("Realizando práctica sobre recursividad");
 		System.out.println(frase);
 		
-		System.out.println("dhhd");
 		
-		invertirPalabra("Hola Ander");
-		
+		System.out.println();
+		System.out.println("Pruebas ejercicio 2:");
+		String invertido;
+		invertido = invertirPalabra3("Hola Ander");
+		System.out.println(invertido);
+		invertido = invertirPalabra3("Hola Ander y Mikel");
+		System.out.println(invertido);
 		
 	}
 	
-	private static void invertirFrase1(String stringInicial, String stringInvertido, int n) {
-		if (n < 0) {
-			System.out.println(stringInvertido);
-			return;
-		}
-		stringInvertido += stringInicial.charAt(n);
-		invertirFrase1(stringInicial, stringInvertido, n-1);
-		//System.out.println(stringInvertido); Para ver el stack de como se van añadiendo las letras
-	}
-	
-	private static String invertirFrase2(String frase) {
+	//PRIMER EJERCICIO:
+	private static String invertirFrase1(String frase) {
 		if (frase.length() <= 1) {
 			return frase; // En el caso base devuelvo esa última letra del string
 		}
 		char charActual = frase.charAt(0);
-		char ultimoChar = frase.charAt(frase.length()-1); //Empieza desde 0
-		//System.out.println(frase.length());
+		char ultimoChar = frase.charAt(frase.length()-1);
 		frase = frase.substring(1, frase.length()-1);
 		//System.out.println(frase);
-		return ultimoChar + invertirFrase2(frase) + charActual;
+		return ultimoChar + invertirFrase1(frase) + charActual;
 		
 	}
 	
-	private static void invertirPalabra(String frase) {
-		System.out.println("juashcdhf");
-		int i = 0;
-		while (frase.charAt(i) != ' ') {
-			i ++;
-			//System.out.println(i+"jasndjn");
+	private static String invertirFrase2(String frase) {
+		if (frase.length() == 0) {
+			return "";
 		}
-		String palabraActual = frase.substring(0,i);
-		System.out.println(palabraActual);
+		char ultimaLetra = frase.charAt(frase.length()-1);
+		frase = frase.substring(0,frase.length()-1);
+		return ultimaLetra + invertirFrase2(frase);
+	}
+	
+	private static String invertirFrase3(String frase) {
+		if (frase.length() == 0) {
+			return "";
+		}
+		return frase.charAt(frase.length()-1) + invertirFrase3(frase.substring(0,frase.length()-1));
+	}
+	
+	//SEGUNDO EJERCICIO:
+	private static String invertirPalabra3(String frase) {
+		if (frase.length() == 0) {
+			return frase;
+		}
+		int i = frase.length() - 1;
+		while (i >= 0 && frase.charAt(i) != ' ') {
+			i --;
+		}
+		String primeraPalabra;
+		if (i < 0) {
+			i = 0;
+			primeraPalabra = frase.substring(i);
+			
+		} else {
+			primeraPalabra = frase.substring(i+1);
+		}
+		frase = frase.substring(0,i);
+//		System.out.println(primeraPalabra);
+//		System.out.println(frase);
+		return primeraPalabra + " " + invertirPalabra3(frase);
 	}
 	
 }
