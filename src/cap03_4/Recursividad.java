4514d461f0ebd2e3f1bcf69649dbfda36665bac1
apala <apala@AnderPalaEnvy>
2023-12-23 00:47:05 +0100

    Práctica 3-4 en proceso

--- src/cap03_4/Recursividad.java
+++ src/cap03_4/Recursividad.java
@@ -1,5 +1,9 @@
 package cap03_4;
 
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Recursividad {
 	
 	public static void main(String[] args) {
@@ -21,9 +25,17 @@ public class Recursividad {
 		invertido = invertirPalabra3("Hola Ander y Mikel");
 		System.out.println(invertido);
 		
+		
+		//Cargo la baraja (el ArrayList) y después llamo al método
+		Carta.cargarCartas();
+		tres(Carta.baraja, 2, "");
+		System.out.println("-----------------------------");
+		tres1(Carta.baraja, 2);
+		//tres1(Carta.baraja, 3);
+		
 	}
 	
-	//PRIMER EJERCICIO: 
+	//PRIMER EJERCICIO:
 	private static String invertirFrase1(String frase) {
 		if (frase.length() <= 1) {
 			return frase; // En el caso base devuelvo esa última letra del string
@@ -54,9 +66,10 @@ public class Recursividad {
 	
 	//SEGUNDO EJERCICIO:
 	private static String invertirPalabra3(String frase) {
-		if (frase.length() == 0) {
-			return frase;
-		}
+		//Con como tengo el código esto lo puedo quitar. SEGUIR AQUI
+//		if (frase.length() == 0) { 
+//			return frase;
+//		}
 		int i = frase.length() - 1;
 		while (i >= 0 && frase.charAt(i) != ' ') {
 			i --;
@@ -64,15 +77,63 @@ public class Recursividad {
 		String primeraPalabra;
 		if (i < 0) {
 			i = 0;
-			primeraPalabra = frase.substring(i);
+			primeraPalabra = frase.substring(i); //Desde 0 hasta el final
+			return primeraPalabra; //Me ahorro una llamada
 			
 		} else {
 			primeraPalabra = frase.substring(i+1);
+			frase = frase.substring(0,i); //Aquí hago hasta i porque todavía no es la última palabra
 		}
-		frase = frase.substring(0,i);
+		/**
+		 * frase = frase.substring(0,i); Al principio estaba aquí, ahora la he puesto arriba
+		 * el else de arriba se ejecuta en todas las llamadas menos en la última SEGUIR AQUI
+		 */
 //		System.out.println(primeraPalabra);
 //		System.out.println(frase);
 		return primeraPalabra + " " + invertirPalabra3(frase);
+	}
+	
+	//TERCER EJERCICIO:
+	private static void tres(ArrayList<Carta> baraja, int n, String concat) {
+		if (n == 0) {
+			System.out.println(concat);
+		} else {
+			for (Carta c: baraja) {
+				tres(baraja, n-1, concat + " y " + c); //Cambiar este string
+			}
+		}
+		
+	}
+	
+	private static LinkedList<Carta> cartasSel = new LinkedList<Carta>(); //Voy a hacerlo con linkedList para quitar el último
+	private static String aConsola = "";
+	
+	private static void tres1(ArrayList<Carta> baraja, int n) {
+		if (n == 0) {
+			return;
+		} else {
+			for (Carta c: baraja) {
+				cartasSel.addLast(c);
+				tres1(baraja, n-1);
+				//Aquí:
+				for (int i = 0; i < cartasSel.size(); i ++) {
+					aConsola += cartasSel.get(i) + " ";
+				}
+				//Esta condición MIRARLA para que no imprima ninguna combinación que no sea de n cartas.
+				//cartas.size()-1 (para simular el removeLast) (probarlo sin el -1 para entenderlo)
+				if (cartasSel.size()-1 > n-1) { //Para no imprimir cuando solo tengo una carta (esto pasa al llegar a la llamada que hace la combinación de una carta con todas las demás)
+					System.out.println(aConsola);
+				}
+				aConsola = "";
+				//System.out.println(cartasSel);
+//				aConsola += cartasSel.getLast() + " ";
+				cartasSel.removeLast();
+			}
+//			cartasSel.clear();
+//			System.out.println(cartasSel + "Este es al arrayList");
+			return;
+		}
+		
 	}
 	
 }
